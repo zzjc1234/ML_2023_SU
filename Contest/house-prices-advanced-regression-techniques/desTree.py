4bcd949a8957eec9d67b0d3d84d3db9f3951ed27
zzjc1234 <2359047351@qq.com>
2023-08-10 19:32:39 +0800

    feat: cross validation

--- Contest/house-prices-advanced-regression-techniques/desTree.py
+++ Contest/house-prices-advanced-regression-techniques/desTree.py
@@ -2,6 +2,7 @@
 from sklearn.tree import DecisionTreeRegressor as dt
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import mean_absolute_error as mae
+from sklearn.model_selection import KFold, cross_val_score
 import pandas as pd
 import numpy as np
 
@@ -21,21 +22,19 @@ train_data = pd.get_dummies(train_data)
 test_data = pd.get_dummies(test_data)
 
 #Get Summary
-print(train_data.describe())
+# print(train_data.describe())
 
 # Process Data
 X=train_data.to_numpy()
 Y=X[:, -1]
 X=X[:, :-1]
-# print(X)
-# print(Y)
 
 #Fit Model
 train_X, val_X, train_y, val_y = tts(X, Y, random_state=1)
-model=dt(random_state=3)
-model.fit(train_X,train_y)
+model=dt()
+k_fold = KFold(n_splits=5, shuffle=True, random_state=42)
+mae_scores = cross_val_score(model, X, Y, scoring='neg_mean_absolute_error', cv=k_fold)
+absolute_mae_scores = -mae_scores
+average_mae = absolute_mae_scores.mean()
 
-#Validation
-val_predictions = model.predict(val_X)
-val_mae = mae(val_predictions, val_y)
-print("Validation MAE: {:,.0f}".format(val_mae))
+print("Average MAE:", average_mae)
