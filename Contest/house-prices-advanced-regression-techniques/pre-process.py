7e0139abbdf0040e2e6bdc1fc3f2b0311708e830
zzjc1234 <2359047351@qq.com>
2023-08-20 15:09:28 +0800

    feat: updata preprocess

--- Contest/house-prices-advanced-regression-techniques/pre-process.py
+++ Contest/house-prices-advanced-regression-techniques/pre-process.py
@@ -17,93 +17,103 @@ def remove_constant_features(data):
 
     return data_without_constants, constant_feature_indices
 
+def import_data():
+# INFO: mark special data
+    column_to_fill =['Alley','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinSF1','FireplaceQu','GarageFinish','GarageCond','PoolQC','Fence','MiscFeature']
+    fill_value = "No"
+
 # INFO: import data
-column_to_fill =['Alley','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinSF1','FireplaceQu','GarageFinish','GarageCond','PoolQC','Fence','MiscFeature']
-fill_value = "No"
+    data = pd.read_csv('~/Desktop/ML_2023_SU/Contest/house-prices-advanced-regression-techniques/train.csv')
 
 # INFO: process Na
-data = pd.read_csv('~/Desktop/ML_2023_SU/Contest/house-prices-advanced-regression-techniques/train.csv')
-data[column_to_fill] = data[column_to_fill].fillna(fill_value)
-data, index=remove_constant_features(data)
-data.dropna(inplace = True)
+    data[column_to_fill] = data[column_to_fill].fillna(fill_value)
+    data, _ =remove_constant_features(data)
+    data.dropna(inplace = True)
 
 # INFO: display the Data
-print(data.describe())
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-print(data.shape)
-print(data.isnull().sum())
+    print(data.describe())
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    print(data.shape)
+    print(data.isnull().sum())
 
-train = pd.get_dummies(data)
-Y = train["SalePrice"]
-X = train.drop('SalePrice', axis = 1)
+    train = pd.get_dummies(data)
+    Y = train["SalePrice"]
+    X = train.drop('SalePrice', axis = 1)
 
-X = X.to_numpy()
-Y = Y.to_numpy()
+    X = X.to_numpy()
+    Y = Y.to_numpy()
+
+    return X,Y
+
+def built_in_test(X,Y):
+# WARNING: Since the variance of dumb variable is really small, these features are removed
+    f_scores, _ = featsel.f_regression(X, Y)
+    mi_scores = featsel.mutual_info_regression(X, Y)
+    chi2_scores = featsel.r_regression(X, Y)
+
+    f_scores/= np.max(f_scores)
+    mi_scores/=np.max(mi_scores)
+    chi2_scores/=np.max(chi2_scores)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression')
+    plt.plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression')
+    plt.plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression')
+
+    plt.xlabel('Feature Index')
+    plt.ylabel('Score')
+    plt.title('Feature Selection Scores Comparison')
+    plt.xticks(range(X.shape[1]), [f'Feature {i}' for i in range(X.shape[1])])
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+# INFO: subplot
+    _, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+
+# Plot F-Regression on the first subplot
+    axes[0, 0].plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression', color='blue')
+    axes[0, 0].set_title('F-Regression')
+    axes[0, 0].set_xlabel('Feature Index')
+    axes[0, 0].set_ylabel('Score')
+    axes[0, 0].legend()
+
+# Plot Mutual Info Regression on the second subplot
+    axes[0, 1].plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression', color='green')
+    axes[0, 1].set_title('Mutual Info Regression')
+    axes[0, 1].set_xlabel('Feature Index')
+    axes[0, 1].set_ylabel('Score')
+    axes[0, 1].legend()
+
+# Plot Chi2 Regression on the third subplot
+    axes[1, 0].plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression', color='orange')
+    axes[1, 0].set_title('Chi2 Regression')
+    axes[1, 0].set_xlabel('Feature Index')
+    axes[1, 0].set_ylabel('Score')
+    axes[1, 0].legend()
+
+# Plot the overlay of the first three plots on the fourth subplot
+    axes[1, 1].plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression', color='blue')
+    axes[1, 1].plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression', color='green')
+    axes[1, 1].plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression', color='orange')
+    axes[1, 1].set_title('Combined Scores')
+    axes[1, 1].set_xlabel('Feature Index')
+    axes[1, 1].set_ylabel('Score')
+    axes[1, 1].legend()
+
+    plt.tight_layout()
+
+    plt.show()
+
+X, Y=import_data()
 
 # INFO: feature selection
 
 # TEST: Built-in test
 
-# WARNING: Since the variance of dumb variable is really small, these features are removed
+built_in_test(X,Y)
 
-f_scores, _ = featsel.f_regression(X, Y)
-mi_scores = featsel.mutual_info_regression(X, Y)
-chi2_scores = featsel.r_regression(X, Y)
-
-f_scores/= np.max(f_scores)
-mi_scores/=np.max(mi_scores)
-chi2_scores/=np.max(chi2_scores)
-
-plt.figure(figsize=(10, 6))
-plt.plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression')
-plt.plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression')
-plt.plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression')
-
-plt.xlabel('Feature Index')
-plt.ylabel('Score')
-plt.title('Feature Selection Scores Comparison')
-plt.xticks(range(X.shape[1]), [f'Feature {i}' for i in range(X.shape[1])])
-plt.legend()
-plt.grid()
-plt.show()
-
-# TODO: subplot
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
-
-# Plot F-Regression on the first subplot
-axes[0, 0].plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression', color='blue')
-axes[0, 0].set_title('F-Regression')
-axes[0, 0].set_xlabel('Feature Index')
-axes[0, 0].set_ylabel('Score')
-axes[0, 0].legend()
-
-# Plot Mutual Info Regression on the second subplot
-axes[0, 1].plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression', color='green')
-axes[0, 1].set_title('Mutual Info Regression')
-axes[0, 1].set_xlabel('Feature Index')
-axes[0, 1].set_ylabel('Score')
-axes[0, 1].legend()
-
-# Plot Chi2 Regression on the third subplot
-axes[1, 0].plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression', color='orange')
-axes[1, 0].set_title('Chi2 Regression')
-axes[1, 0].set_xlabel('Feature Index')
-axes[1, 0].set_ylabel('Score')
-axes[1, 0].legend()
-
-# Plot the overlay of the first three plots on the fourth subplot
-axes[1, 1].plot(range(len(f_scores)), f_scores, marker='o', label='F-Regression', color='blue')
-axes[1, 1].plot(range(len(mi_scores)), mi_scores, marker='s', label='Mutual Info Regression', color='green')
-axes[1, 1].plot(range(len(chi2_scores)), chi2_scores, marker='^', label='Chi2 Regression', color='orange')
-axes[1, 1].set_title('Combined Scores')
-axes[1, 1].set_xlabel('Feature Index')
-axes[1, 1].set_ylabel('Score')
-axes[1, 1].legend()
-
-plt.tight_layout()
-
-plt.show()
 # TEST: Recursive Feature Elimination
 
 
